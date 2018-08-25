cd1af955e9a80d83b8438d1ccc48264d20418449
Casey Zimmer <czimmer@ramapo.edu>
2018-08-25 00:02:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,14 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-  if(number % 3 == 0)
+  
+  if(number % 3 == 0 || number % 5 == 0)
+    {
+      
+    }
+  else
+    {
+      return nil
+    }
+    
 end
